95053185070ca0aeaef62b0b6b3529346b375dca
FelipeElias <soaresfelipe396@gmail.com>
2024-04-10 17:27:17 -0300

    Commit final 2

--- desafio_estagio/resources/js/app.js
+++ desafio_estagio/resources/js/app.js
@@ -1,6 +1,5 @@
 import './bootstrap';
-import '/opt/lampp/htdocs/Desafio_Estagio/desafio_estagio/resources/css/app.css';
-import '/opt/lampp/htdocs/Desafio_Estagio/desafio_estagio/resources/css/ArtistCard.css';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
